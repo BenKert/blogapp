b5024cf03979985289ff342dec921fac3d2354f9
BenKer <benediktas.kertenis@codeacademylt.onmicrosoft.com>
2022-05-24 11:47:40 +0300

    additional file/package structure

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use("/js", express.static(__dirname + "./public/js"));
+app.use("/css", express.static(__dirname + "./public/css"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.use("/api/users", require("./routes/usersRoutes"));
 app.use("/api/blog", require("./routes/blogRoutes"));
 
